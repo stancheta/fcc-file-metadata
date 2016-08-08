824029a640ab4f21b0fcd2a746e5616ee919a7b7
Steven T Ancheta <steventancheta@gmail.com>
2016-08-07 23:40:26 -0700

    Completed file uploading and responding

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 var express = require('express');
 var app = express();
 var multer = require('multer');
-var upload = multer();
+var upload = multer({ dest: 'tmp/' });
 
 var port = process.env.PORT || 8080;
 
 app.use('/', express.static('public'));
+
+app.post('/get-file-size', upload.single('file'), function (req, res) {
+   var response = {};
+   console.log(req.file.originalname);
+   console.log(req.file.size);
+   response.size = req.file.size;
+   res.send(response);
+});
 
 app.listen(port, function() {
    console.log('Now listening on port ' + port);
